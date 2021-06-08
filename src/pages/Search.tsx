50dc6889adfbf05d2f9a4b8cfecc5613a5ba34dc
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-06-08 23:41:00 +0300

    comit message

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   IonContent,
   IonHeader,
@@ -7,11 +8,24 @@ import {
   IonItem,
   IonLabel,
   IonInput,
+  IonText,
+  IonDatetime,
+  IonItemDivider,
+  IonSearchbar,
+  IonFooter,
+  IonButton,
+  useIonAlert,
 } from "@ionic/react";
 
 import CocktailPreviewCard from "../../src/components/CocktailPreviewCart";
+import * as theme from "../theme";
 
 const Tab3: React.FC = () => {
+  const [searchText, setSearchText] = useState("");
+  const [present] = useIonAlert();
+
+  console.log({ theme });
+
   return (
     <IonPage>
       <IonHeader>
@@ -19,7 +33,26 @@ const Tab3: React.FC = () => {
           <IonTitle>Search</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent color="tertiary" fullscreen></IonContent>
+      <IonContent color="tertiary" fullscreen>
+        <div
+          style={{
+            // width: "100%",
+            paddingTop: theme.SIZES.padding * 2,
+            paddingBottom: theme.SIZES.padding * 2,
+          }}
+        >
+          <IonSearchbar
+            color="secondary"
+            value={searchText}
+            placeholder={"Search your cocktail"}
+            onIonChange={(e) => setSearchText(e.detail.value!)}
+          ></IonSearchbar>
+
+          {/* <IonButton slot="end">Shake it!</IonButton> */}
+        </div>
+
+        <IonToolbar>Search Text: {searchText ?? "(none)"}</IonToolbar>
+      </IonContent>
     </IonPage>
   );
 };
