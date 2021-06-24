2689daa55bc49f134bf2643c729e8d10c72148d0
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-06-24 21:02:01 +0300

    added ingredients

--- src/pages/Drink.tsx
+++ src/pages/Drink.tsx
@@ -40,74 +40,76 @@ const DrinkPage: React.FC = (props) => {
   const { isLoading, error, data, isFetching } = useQuery(
     `drink-${props.match.params.drink}`,
     //useQuery returns the drink obj from the location.state, and caches it like it was fetched
-    () => drink,
+    () => {
+      return { drink, ingredients: gatherIngredients(drink) };
+    },
     {
       refetchOnWindowFocus: false,
     }
   );
 
-  let ingredients = gatherIngredients(data);
+  console.log({ data });
 
-  //todo minimize the number this function runs
-
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar color="secondary">
-          <IonButtons slot="start">
-            <IonBackButton />
-          </IonButtons>
-          <IonTitle>{data ? data.strDrink : "Cocktail Page"}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="contentBgBlog" fullscreen>
-        {data ? (
-          <>
+  if (data) {
+    const { drink, ingredients } = data;
+    return (
+      <>
+        <IonPage>
+          <IonHeader>
+            <IonToolbar color="secondary">
+              <IonButtons slot="start">
+                <IonBackButton />
+              </IonButtons>
+              <IonTitle>{drink.strDrink}</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="contentBgBlog" fullscreen>
             <IonHeader collapse="condense">
               <IonToolbar className="drinkPageToolbar">
-                <IonTitle size="large">{drink && drink.strDrink}</IonTitle>
+                <IonTitle size="large">{drink.strDrink}</IonTitle>
               </IonToolbar>
             </IonHeader>
 
             <img
               style={{ padding: 20, borderRadius: "10%" }}
-              src={data && data.strDrinkThumb}
+              src={data.drink && data.drink.strDrinkThumb}
             />
-          </>
-        ) : (
+            <IonList>
+              <IonListHeader lines="inset">
+                <IonLabel>Ingredients</IonLabel>
+              </IonListHeader>
+
+              {ingredients.map((item) => {
+                return (
+                  <IonItem
+                    // button
+                    onClick={() => {
+                      // alert("ckicl");
+                    }}
+                  >
+                    {/*  */}
+                    <IonLabel>
+                      <h3>{item.ingredient}</h3>
+                      <p>{item.measure}</p>
+                    </IonLabel>
+                    <IonCheckbox checked={true} />
+                  </IonItem>
+                );
+              })}
+            </IonList>
+          </IonContent>
+        </IonPage>
+      </>
+    );
+  } else {
+    return (
+      <IonPage>
+        <IonContent className="contentBgBlog">
           <Spinner />
-        )}
-
-        {/* list kai headers */}
-
-        {/*-- List Headers in Lists --*/}
-        <IonList>
-          <IonListHeader lines="inset">
-            <IonLabel>Ingredients</IonLabel>
-          </IonListHeader>
-
-          {ingredients &&
-            ingredients.map((item) => {
-              return (
-                <IonItem
-                  // button
-                  onClick={() => {
-                    // alert("ckicl");
-                  }}
-                >
-                  {/*  */}
-                  <IonLabel>
-                    <h3>{item.ingredient}</h3>
-                    <p>{item.measure}</p>
-                  </IonLabel>
-                  <IonCheckbox checked={true} />
-                </IonItem>
-              );
-            })}
-        </IonList>
-      </IonContent>
-    </IonPage>
-  );
+        </IonContent>
+      </IonPage>
+    );
+  }
 };
 
 export default DrinkPage;
