c54c6dd2039e34419c8e72ee20b2f142f4a33270
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-08-08 01:41:58 +0300

    added grid

--- src/pages/AskBartender.tsx
+++ src/pages/AskBartender.tsx
@@ -6,6 +6,11 @@ import {
   IonText,
   IonTitle,
   IonToolbar,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonItemDivider,
+  IonLabel,
 } from "@ionic/react";
 import * as theme from "../theme";
 import styles from "../pages/askbartender.module.css";
@@ -21,32 +26,32 @@ const AskBartender: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent className="contentBgBlackGray2" fullscreen>
-        {/* <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Ask the Bartender</IonTitle>
-          </IonToolbar>
-        </IonHeader> */}
-        <div className={styles.pageContentContainer}>
-          <div className={styles.prompt} style={{ padding: "30px 20px" }}>
-            <IonText style={{ fontSize: theme.SIZES.h1, color: "white" }}>
-              Out of ideas?
-            </IonText>
-            <br />
-            <br />
-            <IonText
+        <IonGrid style={{ height: "100%", display: "flex" }}>
+          <IonRow style={{ margin: "auto" }}>
+            <IonCol
               style={{
-                fontSize: theme.SIZES.h4,
-                color: "white",
-                marginTop: "100px",
+                display: "flex",
+                flexDirection: "column",
               }}
             >
-              Try answering some simple questions and find that perfect match!
-            </IonText>
-            <IonButton style={{ marginTop: "25px" }} expand="block">
-              Let's Go !
-            </IonButton>
-          </div>
-        </div>
+              <IonText style={{ fontSize: theme.SIZES.h1, color: "white" }}>
+                Out of ideas?
+              </IonText>
+              <IonItemDivider style={{ backgroundColor: "transparent" }} />
+              <IonText
+                style={{
+                  fontSize: theme.SIZES.h4,
+                  color: "white",
+                }}
+              >
+                Try answering some simple questions and find that perfect match!
+              </IonText>
+              <IonButton style={{ marginTop: "25px" }} expand="block">
+                Let's Go !
+              </IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
