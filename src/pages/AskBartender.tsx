4b54c4f325cd8b3faf858ab3d4db0c82aa9a597c
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-08-07 13:34:31 +0300

    test

--- src/pages/AskBartender.tsx
+++ src/pages/AskBartender.tsx
@@ -3,13 +3,15 @@ import {
   IonContent,
   IonHeader,
   IonPage,
+  IonText,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
 import * as theme from "../theme";
 
 const AskBartender: React.FC = () => {
-  const toggle = document.querySelector("#themeToggle");
+  console.log({ theme });
+
   return (
     <IonPage>
       <IonHeader>
@@ -17,16 +19,31 @@ const AskBartender: React.FC = () => {
           <IonTitle>Ask the Bartender</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent
-        className="contentBgBlog"
-        className="contentBgBlackGray2"
-        fullscreen
-      >
+      <IonContent className="contentBgBlackGray2" fullscreen>
         {/* <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Ask the Bartender</IonTitle>
           </IonToolbar>
         </IonHeader> */}
+        <div style={{ padding: "30px 20px" }}>
+          <IonText style={{ fontSize: theme.SIZES.h1, color: "white" }}>
+            Out of ideas?
+          </IonText>
+          <br />
+          <br />
+          <IonText
+            style={{
+              fontSize: theme.SIZES.h4,
+              color: "white",
+              marginTop: "100px",
+            }}
+          >
+            Try answering some simple questions and find that perfect match!
+          </IonText>
+          <IonButton style={{ marginTop: "25px" }} expand="block">
+            Let's Go !
+          </IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
