87ea96336001263504a87b8713b393fea5a5c225
NikosSp <54745415+spiropoulos94@users.noreply.github.com>
2021-08-08 11:35:55 +0300

    added modal

--- src/pages/AskBartender.tsx
+++ src/pages/AskBartender.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   IonButton,
   IonContent,
@@ -11,12 +12,57 @@ import {
   IonRow,
   IonItemDivider,
   IonLabel,
+  IonItem,
+  IonList,
+  IonModal,
 } from "@ionic/react";
 import * as theme from "../theme";
 import styles from "../pages/askbartender.module.css";
 
+const AskBartenderModal: React.FC = ({ showModal, setShowModal }) => {
+  return (
+    <>
+      <IonModal isOpen={showModal} cssClass="my-custom-class">
+        <ion-header translucent>
+          <ion-toolbar>
+            <ion-title>Modal Content</ion-title>
+            <ion-buttons slot="end">
+              <ion-button onclick="dismissModal()">Close</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+        <IonContent fullscreen>
+          <IonList>
+            <IonItem>
+              <ion-label>
+                <h2>Gollum</h2>
+                <p>Sneaky little hobbitses!</p>
+              </ion-label>
+            </IonItem>
+            <IonItem>
+              <ion-label>
+                <h2>Frodo</h2>
+                <p>Go back, Sam! I'm going to Mordor alone!</p>
+              </ion-label>
+            </IonItem>
+            <IonItem>
+              <ion-label>
+                <h2>Samwise</h2>
+                <p>What we need is a few good taters.</p>
+              </ion-label>
+            </IonItem>
+          </IonList>
+        </IonContent>
+        <IonButton onClick={() => setShowModal(false)}>Close Modal</IonButton>
+      </IonModal>
+      <IonButton onClick={() => setShowModal(true)}>Show Modal</IonButton>
+    </>
+  );
+};
+
 const AskBartender: React.FC = () => {
-  //
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <IonPage>
       <IonHeader>
@@ -46,12 +92,17 @@ const AskBartender: React.FC = () => {
               >
                 Try answering some simple questions and find that perfect match!
               </IonText>
-              <IonButton style={{ marginTop: "25px" }} expand="block">
+              <IonButton
+                style={{ marginTop: "25px" }}
+                expand="block"
+                onClick={() => setShowModal(true)}
+              >
                 Let's Go !
               </IonButton>
             </IonCol>
           </IonRow>
         </IonGrid>
+        <AskBartenderModal showModal={showModal} setShowModal={setShowModal} />
       </IonContent>
     </IonPage>
   );
